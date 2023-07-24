5725cfab9097b880248f05614b53659014b31f39
Cristina Mani <misha.mani@yahoo.com>
2023-07-24 22:53:30 +0000

    class Puppy():     def __init__(self, name, favourite_toy):         self.name = name         self.favourite_toy = favourite_toy
        def play(self):
            print(self.name + " is playing with the " + self.favourite_toy)
    
    marble = Puppy('Marble', 'teddy bear')
    marble.play()
    
    onyx = Puppy('Onyx', 'lizard')
    onyx.play()

---
+++ classpractice.py
@@ -0,0 +1,13 @@
+class Puppy():
+    def __init__(self, name, favourite_toy):
+        self.name = name
+        self.favourite_toy = favourite_toy
+
+    def play(self):
+        print(self.name + " is playing with the " + self.favourite_toy)
+
+marble = Puppy('Marble', 'teddy bear')
+marble.play()
+
+onyx = Puppy('Onyx', 'lizard')
+onyx.play()
